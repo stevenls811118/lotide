7080060aaf031b3deae3b31e14bb2432b3c5b935
Yifan Shi <stevenls1118@msn.com>
2022-10-21 19:34:06 -0600

    modified without.js

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,7 +1,7 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const without = require("../without");
 
-/* const words = ["hello", "world", "lighthouse"];
+const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
@@ -9,7 +9,8 @@ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 const numbers = [10, 14, 9, 7, 9, 14, 20];
 console.log(without(numbers, [9, 14, 7, 20])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEqual(numbers, [10, 14, 9, 7, 9, 14, 20]); */
+assertArraysEqual(numbers, [10, 14, 9, 7, 9, 14, 20]);
 
 const num = ["1", "1", "1", "2", "3", "1" , "1"];
 console.log(without(num, ["1", 2, "3"]));
+assertArraysEqual(num, ["1", "1", "1", "2", "3", "1" , "1"]);
