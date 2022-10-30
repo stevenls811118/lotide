f68b5ac5ae705e13eecb4bec9609c9c5ed719a5d
Yifan Shi <stevenls1118@msn.com>
2022-10-30 00:40:19 -0600

    use eqArrays function to compare arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
 const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let boolean = true;
-  if (arrayOne.length === arrayTwo.length) {
-    for (let i in arrayOne) {
-      if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
-        boolean = eqArrays(arrayOne[i], arrayTwo[i]);        
-      } else if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  if (boolean) {
+  // use eqArrays function to compare two arrays to return true or false
+  if (eqArrays(arrayOne, arrayTwo)) {
     console.log("✅✅✅Assertion Passed: ", arrayOne, " === ", arrayTwo);
   } else {
     console.log("🛑🛑🛑Assertion Failed: ", arrayOne, " !== ", arrayTwo);
