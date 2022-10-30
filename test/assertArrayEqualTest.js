99c08b68b45eccc695c4f952104013893f138258
Yifan Shi <stevenls1118@msn.com>
2022-10-30 00:49:48 -0600

    Try the test case assertArraysEqual([1,2], [1,3]) and it works now

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -5,3 +5,5 @@ assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+assertArraysEqual([1,2], [1,3]); // => 🛑🛑🛑Assertion Failed: [1,2] !== [1,3]
