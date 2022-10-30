30325fc3ba166908c61373d7c3527459f65b0ff6
Yifan Shi <stevenls1118@msn.com>
2022-10-30 00:42:44 -0600

    modified line#11 in the else if clause,removed the unnecessary conditions

--- eqObjects.js
+++ eqObjects.js
@@ -4,11 +4,13 @@ const eqObjects = function(object1, object2) {
   let boolean = true;
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key in object1) {
+      // if the values are arrays, use eqArray funtion to compare
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
-      } else if (typeof object1[key] === "object" && typeof object1[key] !== null && Array.isArray(object1[key]) === false && typeof object2[key] === "object" && typeof object2[key] !== null && Array.isArray(object2[key]) === false) {
+      // if the values are object, use eqObjects funtion recursively to compare
+      } else if (typeof object1[key] === "object" && typeof object1[key] !== null && typeof object2[key] === "object" && typeof object2[key] !== null) { 
         boolean = eqObjects(object1[key], object2[key]);
       } else {
         if (object1[key] !== object2[key]) {
